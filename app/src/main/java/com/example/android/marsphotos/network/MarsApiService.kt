407518dea647dbff189e37a94d6fb7ee2fce379c
kiyo <i.mitsuki19@gmail.com>
2023-02-26 17:37:31 +0900

    - jsonデータをkotlinオブジェクトへ変換するためMoshiを追加  - RetrofitのConverterFactoryをMoshiConverterFactoryに変更

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -1,10 +1,14 @@
 package com.example.android.marsphotos.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
 /**
  * Retrofit には、ウェブサービスのベース URI と、
@@ -16,7 +20,7 @@ private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com
  * ScalarsConverterFactory のインスタンスを使ってビルダーで addConverterFactory() を呼び出します。
  */
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
@@ -47,7 +51,7 @@ interface MarsApiService {
      */
     @GET("photos")
     //  suspend 関数にすることで、コルーチン内からこのメソッドを呼び出すことが可能になります
-    suspend fun getPhotos(): String
+    suspend fun getPhotos(): List<MarsPhoto>
 }
 
 
